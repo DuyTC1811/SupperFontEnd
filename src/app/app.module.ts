af2f00ec1ecb50c4008ed8e2f9769e3f2e032e2e
Duy.T.C <duytran.81811@gmail.com>
2023-03-21 22:20:22 +0700

    create: register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
